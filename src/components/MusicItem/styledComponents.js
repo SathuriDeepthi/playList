5afcd24ea0cc8d39b2cb117efa200ef4738048c4
deepthi <sathurideepthi@gmail.com>
2024-04-06 13:28:25 +0000

    solution

---
+++ src/components/MusicItem/styledComponents.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const MusicListItem = styled.li`
+dispaly :flex;
+Justify-content:space-between;
+align-items:center;
+list-style-type:none;
+`
+export const TrackInfoContainer = styled.div`
+dispaly :flex;
+align-items:center;
+width:500px
+`
+export const MusicTrackImage = styled.img`
+heigth:90px;
+width:140px;
+margin-right:20px
+`
+
+export const NameGenreContainer = styled.div``
+
+export const Name = styled.p`
+font-family:'Roboto';
+color:#ffffff;
+font-size:18px;
+font-weight:500;
+`
+
+export const Genre = styled.p`
+color:#3b82f6;
+font-family:'Roboto';
+line-weight:30px
+font-size:18px;
+font-weight:500;
+`
+export const DurationDeleteContainer = styled.div`
+dispaly :flex;
+align-items:center;
+`
+export const Duration = styled.p`
+font-family:'Roboto';
+color:#ffffff;
+font-size:20px;
+font-weight:bold;
+margin-right:50px
+`
+
+export const DeleteButton = styled.button`
+background-colorr:transparent;
+outline:none;
+cursor:pointer;
+border:none;
+color:#ffffff;
+`
---
+++ src/components/MusicPlayList/styledComponents.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div``
+
+export const ArtistBg =styled.div`
+background-image:url("https://assets.ccbp.in/frontend/react-js/music-playlist/music-playlist-Edsheeran-bg.png");
+`
+
+export const ArtistInfo=styled.p``
+
+export const ArtistName = styled. h1``
+
+export const PlayListContainer=styled.div``
+
+
+export const PlayListHeading=styled.h1``
+
+
+export const SearchInput=styled.input``
+
+
+
+export const MusicList=styled.ul``
+
+
+export const EmptyContainer=styled.div``
+
+export const NoSongsText=styled.p``
+
+
